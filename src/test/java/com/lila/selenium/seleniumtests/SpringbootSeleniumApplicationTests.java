1c00f6fa6b6c734a47dea7c0cab0ed2ca6db14f6
ChiaChen <tn00817411@gmail.com>
2025-02-04 14:09:13 +0800

    init

---
+++ src/test/java/com/lila/selenium/seleniumtests/SpringbootSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lila.selenium.seleniumtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSeleniumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
